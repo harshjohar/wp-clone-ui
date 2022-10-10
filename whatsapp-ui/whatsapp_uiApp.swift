a3e783db735414256c67fbc83b56ea7cb0296731
harsh johar <hpsj3112@gmail.com>
2022-10-10 18:00:28 +0530

    Initial Commit

---
+++ whatsapp-ui/whatsapp_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsapp_uiApp.swift
+//  whatsapp-ui
+//
+//  Created by Harsh Johar on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct whatsapp_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
